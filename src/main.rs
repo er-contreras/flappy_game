6b70454a6b1074e18f8c4da3b06b208b7f31b997
Erick Contreras <er.contreras@hotmail.com>
2024-11-04 09:35:26 -0600

    Fix indentation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use  bracket_lib::prelude::*;
+use bracket_lib::prelude::*;
 
 enum GameMode {
     Menu,
@@ -6,9 +6,9 @@ enum GameMode {
     End,
 }
 
-const SCREEN_WIDTH : i32 = 80;
-const SCREEN_HEIGHT : i32 = 50;
-const FRAME_DURATION : f32 = 75.0;
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const FRAME_DURATION: f32 = 75.0;
 
 struct Obstacle {
     x: i32,
@@ -22,39 +22,27 @@ impl Obstacle {
         Obstacle {
             x,
             gap_y: random.range(10, 40),
-            size: i32::max(2, 20 - score)
+            size: i32::max(2, 20 - score),
         }
     }
 
-    fn render(&mut self, ctx: &mut BTerm, player_x : i32) {
+    fn render(&mut self, ctx: &mut BTerm, player_x: i32) {
         let screen_x = self.x - player_x;
         let half_size = self.size / 2;
 
         // Draw the top half of the obstacle
         for y in 0..self.gap_y - half_size {
-            ctx.set(
-                screen_x,
-                y,
-                RED,
-                BLACK,
-                to_cp437('|'),
-            );
+            ctx.set(screen_x, y, RED, BLACK, to_cp437('|'));
         }
 
         // Draw the bottom half of the obstacle
         for y in self.gap_y + half_size..SCREEN_HEIGHT {
-            ctx.set(
-                screen_x,
-                y,
-                RED,
-                BLACK,
-                to_cp437('|'),
-            );
+            ctx.set(screen_x, y, RED, BLACK, to_cp437('|'));
         }
     }
 
     fn hit_obstacle(&self, player: &Player) -> bool {
-        let half_size = self.size /2;
+        let half_size = self.size / 2;
         let does_x_match = player.x == self.x;
         let player_above_gap = player.y < self.gap_y - half_size;
         let player_below_gap = player.y > self.gap_y + half_size;
@@ -78,13 +66,7 @@ impl Player {
     }
 
     fn render(&mut self, ctx: &mut BTerm) {
-        ctx.set(
-            0,
-            self.y,
-            YELLOW,
-            BLACK,
-            to_cp437('@')
-        );
+        ctx.set(0, self.y, YELLOW, BLACK, to_cp437('@'));
     }
 
     fn gravity_and_move(&mut self) {
@@ -144,9 +126,7 @@ impl State {
         self.obstacle.render(ctx, self.player.x);
         if self.player.x > self.obstacle.x {
             self.score += 1;
-            self.obstacle = Obstacle::new(
-                self.player.x + SCREEN_WIDTH, self.score
-            );
+            self.obstacle = Obstacle::new(self.player.x + SCREEN_WIDTH, self.score);
         }
 
         if self.player.y > SCREEN_HEIGHT || self.obstacle.hit_obstacle(&self.player) {
@@ -208,6 +188,6 @@ fn main() -> BError {
     let context = BTermBuilder::simple80x50()
         .with_title("Flappy Dragon")
         .build()?;
-    
+
     main_loop(context, State::new())
 }
